8ff2e22d3fef3e6a22e7236e5d16d4d083ea4e82
DobrevaI <iidobreva24a@gmail.com>
2026-01-22 09:42:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,5 @@
-
+import streamlit as st
+st.title("Survey")
+st.text_input("Please enter your name: ")
+st.number_input("Please enter your age: ")
+subject=st.selectbox("Please select your favorite subject: ", ["Maths", "English", "German", "PE"])
